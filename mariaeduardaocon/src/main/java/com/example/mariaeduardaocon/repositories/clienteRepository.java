7573bcf379a734d85a92754522675fcd938bc8e9
Maria Eduarda <133990702+Maheduardaocon@users.noreply.github.com>
2023-09-26 08:41:00 -0300

    Delete mariaeduardaocon directory

--- mariaeduardaocon/src/main/java/com/example/mariaeduardaocon/repositories/clienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mariaeduardaocon.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.mariaeduardaocon.entities.cliente;
-
-public interface clienteRepository extends JpaRepository<cliente, Long>{
-
-}
